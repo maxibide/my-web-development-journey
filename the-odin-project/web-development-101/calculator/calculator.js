a8f83abe7b8891841c441208b27b5e07105059c0
Maximiliano Bidegain <maxibide@gmail.com>
2021-08-30 20:52:41 -0300

    Finished logic

--- the-odin-project/web-development-101/calculator/calculator.js
+++ the-odin-project/web-development-101/calculator/calculator.js
@@ -1,163 +1,183 @@
 function calculate(a, b, operator) {
-  switch (operator) {
-    case "+":
-      return a + b;
-    case "-":
-      return a - b;
-    case "x":
-      return a * b;
-    case "/":
-      return a / b;
-    default:
-      return "Not a valid operator";
-  }
+    switch (operator) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "x":
+            return a * b;
+        case "/":
+            return a / b;
+        default:
+            return "Not a valid operator";
+    }
 }
 
 let input = {
-  current: [],
-  insertOperand: function (operand) {
-    if (
-      this.current.length === 0 ||
-      this.current[this.current.length - 1].type === "operator"
-    ) {
-      this.current.push({
-        value: [operand],
-        type: "operand",
-      });
-    } else if (this.current[this.current.length - 1].type === "operand") {
-      this.current[this.current.length - 1].value.push(operand);
-    } else if (this.current[this.current.length - 1].type === "result") {
-      this.current = [];
-      this.current.push({
-        value: [operand],
-        type: "operand",
-      });
-    }
+    current: [],
+    insertOperand: function (operand) {
+        if (
+            this.current.length === 0 ||
+            this.current[this.current.length - 1].type === "operator"
+        ) {
+            this.current.push({
+                value: [operand],
+                type: "operand",
+            });
 
-    console.log(this.current); // for debugging
-    this.refreshScreen();
-  },
-  insertOperator: function (operator) {
-    if (this.current.length === 0) {
-      return;
-    } else if (this.current[this.current.length - 1].type === "operator") {
-      this.current[this.current.length - 1].value = operator;
-    } else if (
-      (this.current[this.current.length - 1].type === "operand" &&
-        this.current.filter((e) => e.type === "operator").length === 0) ||
-      this.current[this.current.length - 1].type === "result"
-    ) {
-      this.current.push({
-        value: operator,
-        type: "operator",
-      });
-    } else if (
-      this.current[this.current.length - 1].type === "operand" &&
-      this.current.filter((e) => e.type === "operator").length > 0
-    ) {
-      this.operate();
-      this.current.push({
-        value: operator,
-        type: "operator",
-      });
+        } else if (this.current[this.current.length - 1].type === "operand") {
+            this.current[this.current.length - 1].value.push(operand);
+
+        } else if (this.current[this.current.length - 1].type === "result") {
+            this.current = [];
+            this.current.push({
+                value: [operand],
+                type: "operand",
+            });
+        }
+
+        this.refreshScreen();
+    },
+    insertOperator: function (operator) {
+        if (this.current.length === 0) {
+            return;
+
+        } else if (this.current[this.current.length - 1].type === "operator") {
+            this.current[this.current.length - 1].value = operator;
+
+        } else if (
+            (this.current[this.current.length - 1].type === "operand" &&
+                this.current.filter((e) => e.type === "operator").length === 0) ||
+            this.current[this.current.length - 1].type === "result"
+        ) {
+            this.current.push({
+                value: operator,
+                type: "operator",
+            });
+
+        } else if (
+            this.current[this.current.length - 1].type === "operand" &&
+            this.current.filter((e) => e.type === "operator").length > 0
+        ) {
+            this.operate();
+            this.current.push({
+                value: operator,
+                type: "operator",
+            });
+        }
+
+        this.refreshScreen();
+    },
+    insertFloatPoint: function () {
+        if (this.current.length === 0 || this.current[this.current.length - 1].type === 'operator') {
+            this.current.push({
+                value: ['0', '.'],
+                type: 'operand'
+            });
+
+        } else if (this.current[this.current.length - 1].type === 'operand'
+            && !(this.current[this.current.length - 1].value.some(e => e === '.'))) {
+            this.current[this.current.length - 1].value.push('.')
+
+        } else if (this.current[this.current.length - 1].type === 'result') {
+            this.current = []
+            this.current.push({
+                value: ['0', '.'],
+                type: 'operand'
+            });
+        }
+
+        this.refreshScreen();
+    },
+    operate: function () {
+        let number1 = 0;
+        
+        if (this.current.length === 3) {
+            if (this.current[0].type === "result") {
+                number1 = this.current[0].value;
+            
+            } else {
+                number1 = parseFloat(this.current[0].value.join(""));
+            }
+            let number2 = parseFloat(this.current[2].value.join(""));
+            let result = calculate(number1, number2, this.current[1].value);
+            this.current = [];
+            this.current.push({
+                value: result,
+                type: "result",
+            });
+
+            this.refreshScreen();
+       
+        } else {
+            return;
+        }
+    },
+    refreshScreen: function () {
+        let output = "";
+        const screen = document.querySelector("#screen");
+        input.current.forEach((obj) => {
+            if (typeof obj.value === "object") {
+                output += obj.value.join("") + ' ';
+           
+            } else {
+                output += obj.value + ' ';
+            }
+        });
+        screen.textContent = output;
+    },
+    backspace: function () {
+        if (input.current.length === 0) {
+            return
+       
+        } else if (input.current[input.current.length - 1].type === 'operand') {
+            if (input.current[input.current.length - 1].value.length === 1) {
+                input.current.pop();
+          
+            } else {
+                input.current[input.current.length - 1].value.pop();
+            }
+       
+        } else {
+            input.current.pop();
+        }
+        
+        this.refreshScreen();
+
+    },
+    clear: function () {
+        this.current = [];
+        this.refreshScreen();
     }
-    console.log(this.current); // for debugging
-    this.refreshScreen();
-  },
-  operate: function () {
-    let number1 = 0;
-    if (this.current.length === 3) {
-      if (this.current[0].type === "result") {
-        number1 = this.current[0].value;
-      } else {
-        number1 = parseInt(this.current[0].value.join(""));
-      }
-      let number2 = parseInt(this.current[2].value.join(""));
-      let result = calculate(number1, number2, this.current[1].value);
-      this.current = [];
-      this.current.push({
-        value: result,
-        type: "result",
-      });
-      console.log(this.current); // for debugging
-      this.refreshScreen();
-    } else {
-      return;
-    }
-  },
-  refreshScreen: function () {
-    let output = "";
-    const screen = document.querySelector("#screen");
-    input.current.forEach((obj) => {
-      if (typeof obj.value === "object") {
-        output += obj.value.join("") + ' ';
-      } else {
-        output += obj.value + ' ';
-      }
-    });
-    screen.textContent = output;
-  },
 };
 
 for (let i = 0; i < 10; i++) {
-  document
-    .querySelector(`#operand${i}`)
-    .addEventListener("click", () => input.insertOperand(i));
+    document
+        .querySelector(`#operand${i}`)
+        .addEventListener("click", () => input.insertOperand(i));
 }
 
 document
-  .querySelector("#operatorAdd")
-  .addEventListener("click", () => input.insertOperator("+"));
+    .querySelector("#operatorAdd")
+    .addEventListener("click", () => input.insertOperator("+"));
 document
-  .querySelector("#operatorSubstract")
-  .addEventListener("click", () => input.insertOperator("-"));
+    .querySelector("#operatorSubstract")
+    .addEventListener("click", () => input.insertOperator("-"));
 document
-  .querySelector("#operatorMultiply")
-  .addEventListener("click", () => input.insertOperator("x"));
+    .querySelector("#operatorMultiply")
+    .addEventListener("click", () => input.insertOperator("x"));
 document
-  .querySelector("#operatorDivide")
-  .addEventListener("click", () => input.insertOperator("/"));
-
+    .querySelector("#operatorDivide")
+    .addEventListener("click", () => input.insertOperator("/"));
 document
-  .querySelector("#operatorEqual")
-  .addEventListener("click", () => input.operate());
-
-/*
-
-function refreshScreen() {
-    const screen = document.querySelector("#screen");
-    screen.textContent = input.join('');
-}
-
-function pressEqual() {
-    let operator = "";
-    let index = 0;
-    for (let i = 0; i < operators.length; i++)
-        if (input.some((element) => element === operators[i])) {
-            operator = operators[i];
-            index = input.findIndex((element) => element === operator);
-        }
-    let number1 = input.slice(0, index).join('');
-    let number2 = input.slice(index + 1).join('');
-    let result = operate(parseInt(number1), parseInt(number2), operator);
-    console.log(input);
-    console.log(number1);
-    console.log(number2);
-    console.log(operator);
-    console.log(result);
-    input = result.toString().split('');
-    console.log(input);
-    refreshScreen()
-}
-
-document.querySelector("#clear").addEventListener("click", () => {
-    input = [];
-    console.log(input);
-    refreshScreen();
-});
-
-document.querySelector("#back").addEventListener("click", () => {
-    input.pop();
-    refreshScreen();
-});
-*/
+    .querySelector("#operatorEqual")
+    .addEventListener("click", () => input.operate());
+document
+    .querySelector("#backspace")
+    .addEventListener("click", () => input.backspace());
+document
+    .querySelector("#clear")
+    .addEventListener("click", () => input.clear());
+document
+    .querySelector("#floatingPoint")
+    .addEventListener("click", () => input.insertFloatPoint());
